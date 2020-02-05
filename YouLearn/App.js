fd51ee18c0ddc1eca4c46310715c6565f00961ab
Emma Allegrucci <emmallegrucci@gmail.com>
2020-02-05 05:54:24 -0800

    list created, working on rendering pdfs

--- YouLearn/App.js
+++ YouLearn/App.js
@@ -1,19 +1,38 @@
 import React, {Component} from 'react';
 import { Alert, Button, SectionList, Image,
 	TouchableOpacity, StyleSheet, Text,
-	TextInput, View } from 'react-native';
+	TextInput, View, Dimensions, FlatList } from 'react-native';
 import logo from './assets/logo.png';
+// import Pdf from 'react-native-pdf';
+
+const DATA = [
+	{title: 'Disease Prevention', data:['item1']},
+	{title: 'Feminine Health', data:['item1']},
+	{title: 'Life Skills', data:['item1']},
+	{title: 'Promo Videos', data:['item1']},
+	{title: 'Sexual Education', data:['item1']},
+	{title: 'WASH', data:['item1']},
+	{title: 'ASL Dictionary', data:['item1']},
+];
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.item}>{title}</Text>
+    </View>
+  );
+}
 
 export default class App extends Component {
   	render () {
+  		//const source = {uri: "https://drive.google.com/drive/u/1/folders/1yq9XD0w5PkmPwKrKamabsuqbot_jjE_f"};
 	  	return (
 	    	<View style={styles.container}>
 	    		<View style={styles.rowContainer}>
 		      		<Image source={logo} style={styles.logo}/>
 		      		<TouchableOpacity
-		      		style={styles.button}
-	          		onPress={() => Alert.alert('Menu will pop up')}>
-	          			
+		      			style={styles.button}
+	          			onPress={() => Alert.alert('Menu will pop up')}>
 	          		</TouchableOpacity>
 	          	</View>
 	          	<View style={styles.rowContainer}>
@@ -21,19 +40,12 @@ export default class App extends Component {
 	          	</View>
 	          	<View style={styles.rowContainer}>
 	          		<SectionList
-		          		sections={[
-		          			{title: 'Disease Prevention', data:['item1']},
-		          			{title: 'Feminine Health', data:['item1']},
-		          			{title: 'Life Skills', data:['item1']},
-		          			{title: 'Promo Videos', data:['item1']},
-		          			{title: 'Sexual Education', data:['item1']},
-		          			{title: 'WASH', data:['item1']},
-		          			{title: 'ASL Dictionary', data:['item1']},
-		          		]}
-		          		renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-		          		renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-		          		keyExtractor={(item, index) => index}
-		          	/>
+						sections={DATA}
+						keyExtractor={(item, index) => item + index}
+        				renderItem={({ item }) => <Item title={item} />}
+        				renderSectionHeader={({ section: { title } }) =>
+        					(<Text style={styles.sectionHeader}>{title}</Text>)}
+					/>
 	          	</View>
 	    	</View>
 	  	);
@@ -56,6 +68,7 @@ const styles = StyleSheet.create({
   	button: {
   		flex: 1,
   		margin: 50,
+  		marginTop: 70,
   		padding: 20,
   		alignItems: 'center',
   		borderRadius: 5,
@@ -69,6 +82,7 @@ const styles = StyleSheet.create({
   	logo: {
   		flex: 4,
   		padding: 50,
+  		marginTop: 20,
     	width: 300,
     	height: 100,
     	resizeMode: "contain",
@@ -77,8 +91,7 @@ const styles = StyleSheet.create({
   		backgroundColor: '#dcdcdc',
   		flex: 1,
   		padding: 20,
-  		marginTop: 20,
-  		marginBottom: 20,
+   		marginBottom: 20,
   	},
   	item: {
   		padding: 10,
@@ -93,4 +106,9 @@ const styles = StyleSheet.create({
 	    fontWeight: 'bold',
 	    backgroundColor: 'rgba(247,247,247,1.0)',
 	},
+	// pdf: {
+ //        flex:1,
+ //        width:Dimensions.get('window').width,
+ //        height:Dimensions.get('window').height,
+ //    },
 });
